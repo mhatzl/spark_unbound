c754b1be23ae1b0e31301e8ae9a10aafbc7d262d
Manuel Hatzl <hatzlmanuel@outlook.com>
2021-10-22 19:47:37 +0200

    Fix action test analysis now fully in python

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,23 +1,16 @@
 import sys
 
-fail_exit = 70
-unexpected_exit = 85
+exit_code = 0
 
+with open(sys.argv[1], 'r') as results:
+	for line in results.readlines():
+		line = line.strip()
+		if line.startswith("Failed Assertions:") and not line.endswith("0"):
+		  print("Tests failed!")
+		  exit_code = 70
+	  
+		elif line.startswith("Unexpected Errors:") and not line.endswith("0"):
+			print("Unexpected errors during tests!")
+			exit_code = 85
 
-with open(sys.argv[1], 'r') as fail_file:
-	try:
-		for l in fail_file.readlines():
-			if int(l[0]) !=0:
-				exit(fail_exit) 
-	except:
-		print("Tests failed!")
-		exit(fail_exit)
-
-with open(sys.argv[2], 'r') as unexpected_file:
-	try:
-		for l in unexpected_file.readlines():
-			if int(l[0]) !=0:
-				exit(unexpected_exit) 
-	except:
-		print("Unexpected errors during tests!")
-		exit(unexpected_exit)
+exit(exit_code)
